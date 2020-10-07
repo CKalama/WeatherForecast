b0666d2abec7ad42348f9277072dc5dd9d25f0b5
Chris Kalama <chriskalama96@gmail.com>
2020-10-07 18:34:53 -0500

    Started building logic to store each variable needed with jquery

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Run this function when the document is ready. 
 $(document).ready(function() {
-
+    // Building query URL, added city name as a variable to be able to call later down in script.
     var cityname = "chicago";
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname  + "&units=imperial&appid=3fefd0d7d198dad398a6e3e02a1ee3b5"
 
@@ -10,7 +10,25 @@ $(document).ready(function() {
 }).then(function(response) {
     console.log(queryURL);
 
-    console.log(response);
+    console.log(response.weather.icon);
+
+    // Making Chicago weather appear on HTML. 
+    var weatherDiv = $("#weather-ouput");
+
+    //Storing Weather, What it Feels Like, Wind Speed Humidity, Date, Icon Representation
+    var weather = response.main.temp;
+    var pOne = $('<p>').text("It is currently " + weather);
+    weatherDiv.append(pOne);
+    console.log("It is currently " + weather);
+
+
+    var weatherIcon = response.weather.icon;
+    var image = $('<img>').attr('src', weatherIcon)
+    weatherDiv.append(image);
+
+   
+
+
 
     //var weather = $(this).attr("data-name");
 });
